4bdedefd1b92c0a554ec704897dc6e461b112a93
Helena <helena.soukupova5@gmail.com>
2021-01-05 10:09:19 -0800

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, { useUnifiedTopology: true}, (err, client) => {
-//connect method in order to create connection to the mongo server and access the campsites database
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    //connect method in order to create connection to the mongo server and access the campsites database
 
     assert.strictEqual(err, null); //if it fails it will throw an error argument like if err === null than quit if not, continue
 
@@ -15,21 +16,34 @@ MongoClient.connect(url, { useUnifiedTopology: true}, (err, client) => {
 
     db.dropCollection('campsites', (err, result) => {
         assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
+        console.log('Dropped Collection:', result);
 
-        const collection = db.collection('campsites');
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" },
+            'campsites', result => { //callback function - it will run when it's called at the end - result will be here when it's called
+                console.log('Insert Document:', result.ops);
 
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
-        (err, result) => {
-            assert.strictEqual(err, null);
-            console.log('Insert Document:', result.ops);
+                dboper.findDocuments(db, 'campsites', docs => {
+                    console.log('Found Documents:', docs);
 
-            collection.find().toArray((err, docs) => { //be able to print all and we have here 2 callbacks - insertOne and this but we can do it because it's asynchronous function
-                assert.strictEqual(err, null);
-                console.log('Found Documents:', docs)
+                    dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+                        { description: "Updated Test Description" }, 'campsites',
+                        result => {
+                            console.log('Updated Document Count:', result.result.nModified);
 
-                client.close(); // close app if any issues appeared
+                            dboper.findDocuments(db, 'campsites', docs => {
+                                console.log('Found Documents:', docs);
+
+                                dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                                    'campsites', result => {
+                                        console.log('Deleted Document Count:', result.deletedCount);
+
+                                        client.close();
+                                    }
+                                );
+                            });
+                        }
+                    );
+                });
             });
-        });
     });
 });
